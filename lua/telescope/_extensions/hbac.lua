de1ae58e38e3741919cd845f6f4aa3c4235f490a
momeppkt <peeranut32@gmail.com>
2024-03-21 12:20:58 +0700

    feat(telescope): allows user opts to be pass through telescope extension opts

--- lua/telescope/_extensions/hbac.lua
+++ lua/telescope/_extensions/hbac.lua
@@ -1,6 +1,7 @@
 local telescope = require('telescope')
 
 return telescope.register_extension({
+	setup = require('hbac.config').setup,
 	exports = {
 		buffers = require('hbac.telescope').pin_picker,
 	}
