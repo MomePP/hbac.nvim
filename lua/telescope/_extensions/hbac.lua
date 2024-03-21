0e8e0062993a36c6d536636a24d68621b039207b
momeppkt <peeranut32@gmail.com>
2024-03-21 16:37:25 +0700

    fix(telescope): in case of lazy-loaded from telescope, we're not update only config but need to loads all hbac modules

--- lua/telescope/_extensions/hbac.lua
+++ lua/telescope/_extensions/hbac.lua
@@ -1,7 +1,7 @@
 local telescope = require('telescope')
 
 return telescope.register_extension({
-	setup = require('hbac.config').setup,
+	setup = require('hbac.setup').setup,
 	exports = {
 		buffers = require('hbac.telescope').pin_picker,
 	}
